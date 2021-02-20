ac7aa9ae46cd8a84341019d0e3f9e8a75a95ac54
Bryan Brito <bryansbrito@ufrn.edu.br>
2021-02-19 21:33:46 -0300

    Melhora resultado de funçao seno

--- thread/main_seno.c
+++ thread/main_seno.c
@@ -34,7 +34,7 @@ void *chama_threads(valores_locais *v_locais) {
 
 int main(int argc, char *argv[]) {
   double h, area = 0.0;
-  int threads_size = atoi(argv[1]);
+  int threads_size = atof(argv[1]);
   double n = atof(argv[2]);
   pthread_t threads[threads_size];
   int status;
@@ -42,11 +42,13 @@ int main(int argc, char *argv[]) {
 
   h = (B - A) / n;
 
+  area = (f_seno(A) + f_seno(B)) / 2;
+
   for (int i = 0; i < threads_size; i++) {
 
     status = pthread_create(&threads[i], NULL, (void *)chama_threads,
                             (void *)(size_t)&v_locais);
-
+    
     v_locais[i].local_n = ceil((n / threads_size) * (i + 1));
     v_locais[i].local_a = ceil(h * (i + 1));
     v_locais[i].local_h = h;
